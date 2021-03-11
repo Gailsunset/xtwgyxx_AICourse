ea97815ab512c4702cf935ee40b4f32e3ec59711
Gailsunset <mywu@pku.edu.cn>
2021-03-11 20:00:17 +0800

    第三周课程内容总结

---
+++ 001_教学班级/003_八年级/【第三课】参考程序/车牌号所在地查询（开动脑筋1）.py
@@ -0,0 +1,28 @@
+#汽车牌号所在地查询
+print()
+chepai = input("请输入汽车牌号首字母：").upper()
+if chepai[0] == "A":
+      print("所在地：杭州市")
+elif chepai[0] == "B":
+      print("所在地：宁波市")
+elif chepai[0] == "C":
+      print("所在地：温州市")
+elif chepai[0] == "D":
+      print("所在地：绍兴市")
+elif chepai[0] == "E":
+      print("所在地：湖州市")
+elif chepai[0] == "F":
+      print("所在地：嘉兴市")
+elif chepai[0] == "G":
+      print("所在地：金华市")
+elif chepai[0] == "H":
+      print("所在地：衢州市")
+elif chepai[0] == "J":
+      print("所在地：台州市")
+elif chepai[0] == "K":
+      print("所在地：丽水市")
+elif chepai[0] == "L":
+      print("所在地：舟山市")
+else:
+      print("无法查询")
+
